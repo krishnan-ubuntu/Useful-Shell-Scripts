592323290f25792bd2808c917c7c32f6148478a5
Krishnan <krishnan.ubuntu@gmail.com>
2022-12-11 18:46:55 +0530

    Shell script to configure lamp server with php fpm

--- setup-lamp-server.sh
+++ setup-lamp-server.sh
@@ -2,20 +2,125 @@
 
 #Check if user running this script is root, if not stop execution and print error
 
-sudo apt update
-sudo apt -y install apache2
-sudo apt -y install mysql-server
-#sudo mysql_secure_installation
-sudo apt -y install php libapache2-mod-php php-mysql
-sudo systemctl stop apache2
-sudo a2dismod php7.4
-sudo a2dismod mpm_prefork
-sudo a2enmod mpm_event
-sudo apt -y install php-fpm
-sudo apt -y install libapache2-mod-fcgid
-sudo a2enconf php7.4-fpm
-sudo a2enmod proxy
-sudo a2enmod proxy_fcgi
-sudo apachectl configtest
-sudo systemctl restart apache2
-shutdown -r now;exit
+if [[ $USER != "root" ]]; then
+	echo "This script should be executed as root"
+	exit -1 #-1 returns error here
+fi
+
+# Update the packages
+
+apt update
+
+
+# Install apache web server
+
+apt -y install apache2
+
+
+# Install mysql server
+
+apt -y install mysql-server
+
+
+# Install php and other required packages
+
+apt -y install php libapache2-mod-php php-mysql
+
+
+# Getting php version for future commands
+
+CURRENT_VERSION=$(php -v | head -n 1 | cut -d " " -f 2 | cut -f1-2 -d".")
+
+
+# Installing memcached
+
+apt-get -y install memcached
+
+
+# Installing php memcached package
+
+apt-get -y install php"$CURRENT_VERSION"-memcached
+
+
+# Installing php xml package
+
+apt-get install php"$CURRENT_VERSION"-xml
+
+
+# Restart apache
+
+systemctl restart apache2
+
+
+# Now we will configure php-fpm to ensure the application performance is faster
+
+
+# Stopping apache
+
+systemctl stop apache2
+
+
+# Disabling php 7.4
+
+a2dismod php"$CURRENT_VERSION"
+
+
+# Disabling mpm_prefork module.
+
+a2dismod mpm_prefork
+
+
+# Enabling mpm_event. This module increases the speed and performance of apache
+
+a2enmod mpm_event
+
+
+# Installing php-fpm
+
+apt -y install php-fpm
+
+
+# Installing mod-fcgid, it is a faster alternate to mod_cgi
+
+apt -y install libapache2-mod-fcgid
+
+
+# Enabling required php configuration for fpm
+
+a2enconf php"$CURRENT_VERSION"-fpm
+
+
+# Enabling proxy
+
+a2enmod proxy
+
+
+# Enabling proxy_fcgi
+
+a2enmod proxy_fcgi
+
+
+
+# Getting the apache config value
+
+APACHE_CONFIG_CHECK=$(apachectl configtest)
+
+
+# Checking if apache configuration is ok 
+
+if [[ APACHE_CONFIG_CHECK != "OK" ]]; then
+	echo "This script encountered an error with apache configuration"
+	exit -1
+fi
+
+
+# Restart apache server
+
+systemctl restart apache2
+
+
+# shutdown the server and exit without error
+
+shutdown now;exit
+
+
