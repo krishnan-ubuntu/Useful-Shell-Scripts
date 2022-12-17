956592367523bcb713a11f967a8a07dcc9360757
Krishnan <krishnan.ubuntu@gmail.com>
2022-12-17 17:44:14 +0530

    script to check domain name of choice

--- check-website-availability.sh
+++ check-website-availability.sh
@@ -4,10 +4,13 @@
 
 echo "Please enter the domain name"
 read TARGET
+
 echo -e "\nChecking....\n"
+
 # -I to show document info only and not fetch the page details
 # The -f option in curl to check server errors like HTTP 404 should fail too
-if curl -I -f "https://$TARGET" > /dev/null; then
+# /dev/null will prevent curl from printing the result on screen
+if curl -I -f "https://$TARGET" > /dev/null; then 
   echo -e "\n$TARGET is active\n"
 else
   echo -e "\n$TARGET is down\n"
